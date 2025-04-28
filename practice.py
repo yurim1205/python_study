a3b055b6dfa8888b057f51eb62a0b43cb73933a9
yurim1205 <eoulim3237@naver.com>
2025-04-28 18:55:01 +0900

    기초

--- practice.py
+++ practice.py
@@ -27,3 +27,8 @@ print("우리집  " + animal + "의 이름은 " +name+ "이에요")
 print(name + "이는 " + str(age) + "살이며, " +hobby+"를 아주 좋아해요")
 print(name+"이는 어른일까요? " +str(is_adult))
 
+# 연산자
+print(1+1)
+print(5*2)
+print(2**3)
+print(5%3)
