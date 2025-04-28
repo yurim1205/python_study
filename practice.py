246fca1b144e4ca39ac7b16aa0dc8df34bcebe70
yurim1205 <eoulim3237@naver.com>
2025-04-28 17:44:59 +0900

    기본 세팅

---
+++ practice.py
@@ -0,0 +1,29 @@
+# 숫자 자료형 
+print(5)
+print(-10)
+print(3.14)
+
+# 문자열 자료형
+print('풍선') 
+print("나비")
+print("ㅋ"*9)
+
+# boolean 자료형 (참 / 거짓)
+print(5<10)
+print(False)
+print(True)
+print(not True)
+print(not False)
+print(not (5>10))
+
+# 변수
+animal = "토끼"
+name = "토실"
+age = 2
+hobby = "먹기"
+is_adult = age >= 1
+
+print("우리집  " + animal + "의 이름은 " +name+ "이에요")
+print(name + "이는 " + str(age) + "살이며, " +hobby+"를 아주 좋아해요")
+print(name+"이는 어른일까요? " +str(is_adult))
+
